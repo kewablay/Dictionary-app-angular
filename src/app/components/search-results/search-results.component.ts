838bd373dc68f9d9993b9290d1fb5e94b6306bac
kewablay <kojokewajunior123@gmail.com>
2024-07-18 09:16:55 +0000

    feat: added play audio and error state for search input

--- src/app/components/search-results/search-results.component.ts
+++ src/app/components/search-results/search-results.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MeaningsListComponent } from '../meanings-list/meanings-list.component';
+import { DictionaryService } from '../../services/dictionary.service';
 
 @Component({
   selector: 'app-search-results',
@@ -9,75 +10,26 @@ import { MeaningsListComponent } from '../meanings-list/meanings-list.component'
   styleUrl: './search-results.component.sass',
 })
 export class SearchResultsComponent {
-  searchResults: any = [
-    {
-      word: 'keyboard',
-      phonetic: '/ˈkiːbɔːd/',
-      phonetics: [
-        {
-          text: '/ˈkiːbɔːd/',
-          audio: '',
-        },
-        {
-          text: '/ˈkiːbɔːd/',
-          audio: '',
-        },
-        {
-          text: '/ˈkibɔɹd/',
-          audio:
-            'https://api.dictionaryapi.dev/media/pronunciations/en/keyboard-us.mp3',
-          sourceUrl: 'https://commons.wikimedia.org/w/index.php?curid=1755168',
-          license: {
-            name: 'BY-SA 3.0',
-            url: 'https://creativecommons.org/licenses/by-sa/3.0',
-          },
-        },
-      ],
-      meanings: [
-        {
-          partOfSpeech: 'noun',
-          definitions: [
-            {
-              definition:
-                '(etc.) A set of keys used to operate a typewriter, computer etc.',
-              synonyms: [],
-              antonyms: [],
-            },
-            {
-              definition:
-                'A component of many instruments including the piano, organ, and harpsichord consisting of usually black and white keys that cause different tones to be produced when struck.',
-              synonyms: [],
-              antonyms: [],
-            },
-            {
-              definition:
-                'A device with keys of a musical keyboard, used to control electronic sound-producing devices which may be built into or separate from the keyboard device.',
-              synonyms: [],
-              antonyms: [],
-            },
-          ],
-          synonyms: ['electronic keyboard'],
-          antonyms: [],
-        },
-        {
-          partOfSpeech: 'verb',
-          definitions: [
-            {
-              definition: 'To type on a computer keyboard.',
-              synonyms: [],
-              antonyms: [],
-              example: 'Keyboarding is the part of this job I hate the most.',
-            },
-          ],
-          synonyms: [],
-          antonyms: [],
-        },
-      ],
-      license: {
-        name: 'CC BY-SA 3.0',
-        url: 'https://creativecommons.org/licenses/by-sa/3.0',
-      },
-      sourceUrls: ['https://en.wiktionary.org/wiki/keyboard'],
-    },
-  ];
+  searchResults: any;
+
+  constructor(private dictionaryService: DictionaryService) {}
+
+  ngOnInit(): void {
+    this.dictionaryService.getSearchResults().subscribe((results) => {
+      this.searchResults = results;
+      console.log("results: ", this.searchResults);
+    });
+  }
+
+  playAudio() {
+    if (this.searchResults && this.searchResults[0]?.phonetics) {
+      const audioUrl = this.searchResults[0].phonetics.find((phonetic: any) => phonetic.audio)?.audio;
+      if (audioUrl) {
+        const audio = new Audio(audioUrl);
+        audio.play();
+      } else {
+        alert('Audio not available for this word.');
+      }
+    }
+  }
 }
--- src/app/services/dictionary.service.ts
+++ src/app/services/dictionary.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DictionaryService {
+  private apiUrl = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
+  private searchResultsSubject = new BehaviorSubject<any>(null);
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  searchWord(query: string): Observable<any> {
+    return this.http
+      .get<any>(`${this.apiUrl}${query}`)
+      .pipe(tap((results) => this.searchResultsSubject.next(results)));
+  }
+  
+
+  getSearchResults(): Observable<any> {
+    return this.searchResultsSubject.asObservable();
+  }
+
+  testSearch() {
+    this.searchWord('keyboard');
+  }
 }
